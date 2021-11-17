5cbfc1bb157ab2c2870bcd6fe64ca9df59a3b770
JohnViviano <john@startin.com>
2021-11-16 20:30:05 -0600

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#hello'
   get 'static_pages/hello', to: 'static_pages#hello'
 end
